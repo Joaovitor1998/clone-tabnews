c842a3acdb56159e11d544ad7a2a5b858efc865e
João Vitor <joaovitorf1998oliveira@gmail.com>
2024-08-12 21:57:44 -0300

    Teste deploy com Vercel

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Olha o curso, minha beluguinha!</h1>
+    return <>
+        <h1>Saudade de você, beluguinha turca!</h1>
+        <img src="https://i.pinimg.com/564x/17/b3/bd/17b3bdbfc474668babc9b958f8c37a48.jpg"></img>
+    </>
 }
 
 export default Home;
