ea3152943ee107dc2365721010536907da8ef106
João Vitor <joaovitorf1998oliveira@gmail.com>
2024-08-13 15:01:27 -0300

    Adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório do curso do Felipe Deschamps, clonando o TabNews.
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 function Home() {
-    return <>
-        <h1>Saudade de você, beluguinha turca!</h1>
-        <img src="https://i.pinimg.com/564x/17/b3/bd/17b3bdbfc474668babc9b958f8c37a48.jpg"></img>
+  return (
+    <>
+      <h1>Saudade de você, beluguinha turca!</h1>
+      <img src="https://i.pinimg.com/564x/17/b3/bd/17b3bdbfc474668babc9b958f8c37a48.jpg"></img>
     </>
+  );
 }
 
 export default Home;
