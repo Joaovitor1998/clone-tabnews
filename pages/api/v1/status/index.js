c1d39cd317cad0eca1b1e3b2893b09ccfd736bef
João Vitor Ferreira <joaovitorf1998oliveira@gmail.com>
2024-08-15 22:54:27 -0300

    Finished `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,29 @@
 import database from "infra/database.js"
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 AS sum;")
-  console.log(result.rows)
-  response.status(200).json({ status: "Está funcionando" });
+  const dbVersionResult = await database.query("SHOW server_version;")
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConnectionsResult = await database.query("SHOW max_connections;")
+  const dbMaxConnectionsValue = parseInt(dbMaxConnectionsResult.rows[0].max_connections);
+  
+  const databaseName = process.env.POSTGRES_DB;
+  const dbOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  })
+  const dbOpenedConnectionsValue = dbOpenedConnectionsResult.rows[0].count;
+
+  const updatedAt = new Date().toISOString()
+
+  response.status(200).json({ 
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: dbMaxConnectionsValue,
+        opened_connections: dbOpenedConnectionsValue
+      }
+    }
+  });
 }
--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -1,8 +1,31 @@
 describe("/api/v1/status", () => {
   describe("GET", () => {
+
     it("should return Status Code 200", async () => {
       const response = await fetch("http://localhost:3000/api/v1/status");
+      const responseBody = await response.json();
+
+      const { 
+        updated_at, 
+        dependencies: { 
+          database: {
+            version, 
+            max_connections, 
+            opened_connections
+          } 
+        }
+      } = responseBody;
+    
+      const parsedData = new Date(updated_at).toISOString()
+      expect(updated_at).toEqual(parsedData)
+
+      expect(version).toEqual("16.0")
+      expect(max_connections).toEqual(100)
+      expect(opened_connections).toEqual(1)
+
       expect(response.status).toBe(200);
     });
+    
+
   });
 });
